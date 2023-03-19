f59243eb78d57d379044a7eb7281be0458752daa
sophie0714 <jpar875@aucklanduni.ac.nz>
2023-03-19 21:38:49 +1300

    Modify the letter case of name

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -16,16 +16,23 @@ public class InsuranceSystem {
   }
 
   public void createNewProfile(String userName, String age) {
+
+    // Capitalise the first letter and decapitalise the rest of letters
+    userName = userName.substring(0,1).toUpperCase() + userName.substring(1).toLowerCase();
+
     if (userName.length()<3){ 
       // Check if the username is shorter than 3, then print error message
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+
     }else if (Integer.valueOf(age) < 0){
       // Check if the age is not a positive integer, then print error message
       MessageCli.INVALID_AGE.printMessage(age,userName);
+
     }else{
        // Store a typed profile of a client in the array list
       Client c1 = new Client(userName, Integer.valueOf(age));
       listOfClients.add(c1);
+      
       // Print message of successful profile creation
       MessageCli.PROFILE_CREATED.printMessage(userName,age);
     }
