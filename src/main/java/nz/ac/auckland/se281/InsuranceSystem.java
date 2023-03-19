2d29774f113159164ca97adb0132591b52ecf197
sophie0714 <jpar875@aucklanduni.ac.nz>
2023-03-19 21:27:55 +1300

    Check the length of name and age validity before adding a profile into 'listOfClients' and print appropriate messages

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -16,11 +16,19 @@ public class InsuranceSystem {
   }
 
   public void createNewProfile(String userName, String age) {
-    
-    // Store a typed profile of a client in the array list
-    Client c1 = new Client(userName, Integer.valueOf(age));
-    listOfClients.add(c1);
-    
+    if (userName.length()<3){ 
+      // Check if the username is shorter than 3, then print error message
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+    }else if (Integer.valueOf(age) < 0){
+      // Check if the age is not a positive integer, then print error message
+      MessageCli.INVALID_AGE.printMessage(age,userName);
+    }else{
+       // Store a typed profile of a client in the array list
+      Client c1 = new Client(userName, Integer.valueOf(age));
+      listOfClients.add(c1);
+      // Print message of successful profile creation
+      MessageCli.PROFILE_CREATED.printMessage(userName,age);
+    }
   }
 
   public void loadProfile(String userName) {
