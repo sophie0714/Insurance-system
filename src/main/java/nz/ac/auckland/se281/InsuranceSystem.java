de4161649a3730d4a55b1592156e56d2c92593f6
sophie0714 <jpar875@aucklanduni.ac.nz>
2023-03-19 20:43:36 +1300

    Create an arraylist named 'listOfClients' and store  profiles in the arraylist

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,8 +1,12 @@
 package nz.ac.auckland.se281;
 
 import nz.ac.auckland.se281.Main.PolicyType;
+import java.util.ArrayList;
 
 public class InsuranceSystem {
+  // Create an array list where all clients profiles are stored
+  public ArrayList<Client> listOfClients = new ArrayList<>();
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
@@ -12,7 +16,11 @@ public class InsuranceSystem {
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: Complete this method.
+    
+    // Store a typed profile of a client in the array list
+    Client c1 = new Client(userName, Integer.valueOf(age));
+    listOfClients.add(c1);
+    
   }
 
   public void loadProfile(String userName) {
