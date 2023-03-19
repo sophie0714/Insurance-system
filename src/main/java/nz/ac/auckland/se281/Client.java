4dd21c89d13d64ea825d47c48f603772b8db5606
sophie0714 <jpar875@aucklanduni.ac.nz>
2023-03-19 19:40:04 +1300

    Create and construct a new class named 'Client' git log

---
+++ src/main/java/nz/ac/auckland/se281/Client.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se281;
+
+public class Client {
+    //Instance fields
+    private  String name;
+    private int age;
+
+    //Construct the class
+    public Client(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
