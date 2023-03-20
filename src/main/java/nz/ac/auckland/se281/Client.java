0d5a0bd73924a7542ed88d8f6a0d76c666737ee3
sophie0714 <jpar875@aucklanduni.ac.nz>
2023-03-20 13:46:40 +1300

    Fix formatting style

--- src/main/java/nz/ac/auckland/se281/Client.java
+++ src/main/java/nz/ac/auckland/se281/Client.java
@@ -1,23 +1,23 @@
 package nz.ac.auckland.se281;
 
 public class Client {
-    //Instance fields
-    private  String name;
-    private String age;
+  // Instance fields
+  private String name;
+  private String age;
 
-    //Construct the class
-    public Client(String name, String age){
-        this.name = name;
-        this.age = age;
-    }
+  // Construct the class
+  public Client(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
 
-    //Get attribute,name, in a type client outside of this class
-    public String getName(){
-        return this.name;
-    }
+  // Get attribute,name, in a type client outside of this class
+  public String getName() {
+    return this.name;
+  }
 
-    //Get attribute, age, in a type client outside of this class
-    public String getAge(){
-        return this.age;
-    }
+  // Get attribute, age, in a type client outside of this class
+  public String getAge() {
+    return this.age;
+  }
 }
--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,7 +1,7 @@
 package nz.ac.auckland.se281;
 
-import nz.ac.auckland.se281.Main.PolicyType;
 import java.util.ArrayList;
+import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
 
@@ -12,91 +12,87 @@ public class InsuranceSystem {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
-
   public void printDatabase() {
 
     // When there is no clients in the listOfClients
-    if (listOfClients.size() == 0){
+    if (listOfClients.size() == 0) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
 
-    // When there is only one client's profile in the listOfClients
-    }else if (listOfClients.size()== 1){
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1","",":");
+      // When there is only one client's profile in the listOfClients
+    } else if (listOfClients.size() == 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
 
-    // When there are more than two clients' profiles in the listOfClients
-    }else{
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(listOfClients.size()),"s",":");
+      // When there are more than two clients' profiles in the listOfClients
+    } else {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(
+          Integer.toString(listOfClients.size()), "s", ":");
     }
 
-
     // Print all existing clients' profiles in database
-    for (int i=0;i<listOfClients.size();i++){
+    for (int i = 0; i < listOfClients.size(); i++) {
       Client aClient = listOfClients.get(i);
       String aClientName = aClient.getName();
       String aClientAge = aClient.getAge();
-      System.out.print((i+1) + ": ");
-      System.out.println(aClientName + ", " + aClientAge); 
+      System.out.print((i + 1) + ": ");
+      System.out.println(aClientName + ", " + aClientAge);
     }
   }
-
 
   public void createNewProfile(String userName, String age) {
 
     // Capitalise the first letter and decapitalise the rest of letters
-    userName = userName.substring(0,1).toUpperCase() + userName.substring(1).toLowerCase();
+    userName = userName.substring(0, 1).toUpperCase() + userName.substring(1).toLowerCase();
 
     // Determine if a name is unique or not
     // Initialise alreadyExist to false
     boolean alreadyExist = false;
 
     // Check if newly typed name is alread in the database
-    for (int i=0; i<listOfClients.size(); i++){
+    for (int i = 0; i < listOfClients.size(); i++) {
       Client aClient = listOfClients.get(i);
       String aClientName = aClient.getName();
 
       // If exist, alreadyExist becomes true, meaning the name is not unique
-      if (userName.equals(aClientName)){
+      if (userName.equals(aClientName)) {
         alreadyExist = true;
       }
     }
 
-    //Determine if age is an integer or not
-    //Initialise isInteger to true
+    // Determine if age is an integer or not
+    // Initialise isInteger to true
     boolean isInteger = true;
 
     // Check if age is numerical integer or not with ascii value of each character
-    for (int i=0; i<age.length(); i++){
+    for (int i = 0; i < age.length(); i++) {
       char digit = age.charAt(i);
       // 48 ascii '0', 67 ascii '9'
-      if ((int)digit < 48 || (int)digit > 67){
+      if ((int) digit < 48 || (int) digit > 67) {
         // Age is not an integer
         isInteger = false;
       }
     }
-    
 
-    if (userName.length()<3){ 
+    if (userName.length() < 3) {
       // Check if the username is shorter than 3, then print error message
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
 
-    }else if (isInteger == false || Integer.valueOf(age) < 0){
+    } else if (isInteger == false || Integer.valueOf(age) < 0) {
       // Check if the age is not a positive integer, then print error message
-      MessageCli.INVALID_AGE.printMessage(age,userName);
+      MessageCli.INVALID_AGE.printMessage(age, userName);
 
-    }else if(alreadyExist){
+    } else if (alreadyExist) {
       // Check if the name is already in the database, then print error message
       MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
 
-    }else{
-       // Store a typed profile of a client in the array list
+    } else {
+      // Store a typed profile of a client in the array list
       Client c1 = new Client(userName, age);
       listOfClients.add(c1);
 
       // Print message of successful profile creation
-      MessageCli.PROFILE_CREATED.printMessage(userName,age);
+      MessageCli.PROFILE_CREATED.printMessage(userName, age);
     }
   }
-
 
   public void loadProfile(String userName) {
     // TODO: Complete this method.
